1a3d769c6324b4194a24689715a294e7b75a479f
phil-blu <66381128+phil-blu@users.noreply.github.com>
2021-02-19 01:35:45 +0000

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,5 +1,6 @@
 '''
-Write a function that takes in a non-empty string and that returns a boolean representing whether the string is a palindrome. A palindrome is defined as a string that's written the same way forward and backward. Note that single-character strings are palindromes.
+Write a function that takes in a non-empty string and that returns a boolean representing whether the string is a palindrome. 
+A palindrome is defined as a string that's written the same way forward and backward. Note that single-character strings are palindromes.
 string = "abcdcba"
 '''
 
