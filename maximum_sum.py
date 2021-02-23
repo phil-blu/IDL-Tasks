bdd52981e8a34f7c425c0bde2605e041fcbb4964
dnjoe96 <donjoedbest@gmail.com>
2021-02-23 05:56:38 +0100

    adding a function to obtain the max sum of adjacent numbers in a list

--- maximum_sum.py
+++ maximum_sum.py
@@ -1,39 +1,51 @@
-'''
-Write a function that takes in an array of positive integers and returns the maximum sum of
-non-adjacent elements in the array.
-If the input array is empty, the function should return 0.
+# Write a function that takes in a non-empty array of integers and return the maximum 
+# sum that can be obtained by summing up all of the integers in a non-empty subarray 
+# of the input array. A subarray must only contain adjacent numbers (numbers next to each other in the input array).
+# array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
 
-array = [75, 105, 120, 75, 90, 135]
-'''
 
-Array = [75, 105, 120, 75, 90, 135]
-A = []
-def maximum_sum(array):
-    #handling exception of an empty array
-    if array == []:
+def maximum_sum(a_list):
+    
+    
+    lenth = len(a_list)
+    
+    # A case for when the input list is empty
+    if lenth == 0:
         return 0
 
-    #Gets the sum of none adjacent elements
-    else:
-        even_sum, odd_sum = 0, 0
-        for i in array[::2]:
-            even_sum += i
-        for i in array[1::2]:
-            odd_sum += i
+    # initializing the maximum sum to be zero
+    max_sum = 0
+
+    # looping through the range of the the list indices
+    for one in range(lenth):
+
+        ### obtaining the sublist
+        
+        if one == 0:   # for the first item in the list which should have one adjacent number
+            sub_list = a_list[one:one+2]
+        
+        elif one == lenth - 1:  # for the last item in the list which should have one adjacent number
+            sub_list = a_list[one -1:one + 1]
+
+        else: # for the general case where every other item in between has an adjacent number to the left and right.
+            sub_list = a_list[one - 1: one + 2]
+
+        ### getting sum of sub_list
+        
+        the_sum = 0  # initializing the sum as zero, then we tranverse through
+        for item in sub_list:
+            the_sum = the_sum + item   # summing up elements of the list
+        print(one, sub_list, the_sum)
+        
+        if max_sum < the_sum:
+            max_sum = the_sum
+
+    return max_sum
 
 
-        #Checks for maximum value
-        if even_sum > odd_sum:
-            return even_sum
-        elif even_sum < odd_sum:
-            return odd_sum
-        elif even_sum == odd_sum:
-            return None
 
-print(maximum_sum(Array))
 
-'''
-COMPLEXITY ANALYSIS
-O(2n) for time which is O(n) for time
-O(1) for space
-'''
+   
+
+a_list = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+fn = maximum_sum(a_list)
